9990df20287564d75844f56acb16457ae6e08efb
jeffrey torres <alto.951@hotmail.com>
2022-09-03 23:45:53 -0500

    Blur box solved

--- main.go
+++ main.go
@@ -21,16 +21,16 @@ func main() {
 }
 
 func solution(image [][]int) [][]int {
+	// 1. start calc 3x3 center indices i.e for 5x5 image [1,1]	[1,2] [1,3] [2,1][2,2][2,3]
+	// 2. from center indices calc borders
+	// 3. calc avg
 	imgOut := [][]int{}
-	totalGroups := len(image[0])
-	rowsCount := len(image)
 	colsCount := len(image[0])
+	rowsCount := len(image)
 
-	fmt.Println(rowsCount, colsCount)
-
-	for i := 1; i < totalGroups-1; i++ {
+	for i := 1; i < rowsCount-1; i++ {
 		rowAvg := []int{}
-		for i2 := 1; i2 < totalGroups-1; i2++ {
+		for i2 := 1; i2 < colsCount-1; i2++ {
 			sum := 0
 			for j := 0; j < 3; j++ {
 				for k := 0; k < 3; k++ {
